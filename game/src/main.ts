e04f68cc42ab607e87dfafc3ada6bff8ccac7e05
Romain Jacq <rjacq@z2r12p5.42lyon.fr>
2025-03-19 16:41:08 +0100

    add shader

--- game/src/main.ts
+++ game/src/main.ts
@@ -23,13 +23,13 @@ class Game {
 		this.engine = new Engine(canvas, true);
 		this.scene = new Scene(this.engine);
 
-		this.camera = new ArcRotateCamera("camera", Math.PI / 2, Math.PI / 2, 40, Vector3.Zero(), this.scene);
+		this.camera = new ArcRotateCamera("camera", Math.PI * 1.5, Math.PI / 3, 10, Vector3.Zero(), this.scene);
 		this.camera.attachControl(canvas, true);
 		new HemisphericLight("light", new Vector3(0, 1, 0), this.scene);
 
-		const arenaMesh = MeshBuilder.CreateDisc("arenaDisc", { radius: calculateArenaRadius(100), tessellation: 128 }, this.scene);
+		const arenaMesh = MeshBuilder.CreateDisc("arenaDisc", { radius: calculateArenaRadius(2), tessellation: 128 }, this.scene);
 		const material = new StandardMaterial("arenaMaterial", this.scene);
-		material.diffuseColor.set(0, 0, 0);
+		material.diffuseColor.set(0.75, 0.75, 0.75);
 		arenaMesh.rotation.x = Math.PI / 2;
 		arenaMesh.material = material;
 		const ballBaseMesh = MeshBuilder.CreateSphere("ballBase", { diameter: 0.5 }, this.scene);
@@ -38,14 +38,147 @@ class Game {
 		const paddleBaseMesh = MeshBuilder.CreateCylinder("goal", {height: 0.5, diameter:1, subdivisions:16}, this.scene);
 		paddleBaseMesh.setEnabled(true);
 		paddleBaseMesh.setPivotPoint(Vector3.Zero());
+		const mat = new StandardMaterial("paddleMaterial", this.scene);
+		mat.diffuseColor.set(1, 0, 0);
+		paddleBaseMesh.material = mat;
 		const shieldBaseMesh = MeshBuilder.CreateCylinder("shield", {height: 0.25, diameter: 1.65, tessellation: 12, arc: 0.5, enclose: true, updatable: true}, this.scene);
 		shieldBaseMesh.setEnabled(true);
 		shieldBaseMesh.setPivotPoint(Vector3.Zero());
-		const paddleShader = new ShaderMaterial("paddleShader", this.scene, "./shader/shield.vertex.fx",{
-			attributes: ["position"],
-			uniforms: ["worldViewProjection", "shieldAngle", "baseAngle", "cylinderCenter", "cylinderAxis", "intensity"]
+
+		// Effect.ShadersStore['customVertexShader'] = `precision highp float;
+
+		// 	attribute vec3 position;
+		// 	attribute float shieldAngle;
+		// 	attribute float isActive;
+		// 	// uniform mat4 world;
+		// 	uniform mat4 worldViewProjection;
+		// 	// uniform float baseAngle; //radian
+
+		// 	uniform vec3 cylinderCenter;
+		// 	uniform vec3 cylinderAxis;
+		// 	uniform float intensity;
+
+
+		// 	void main() {
+		// 		float size = 1.0;
+		// 		vec3 center = vec3(0, 0, 0);  // Centre du cylindre
+		// 		vec3 axe = vec3(0, 1, 0);  // Axe de rotation (Y)
+		// 		float intense = 0.0;  // Intensité de la rotation
+		// 		vec3 P = position;  // Position du vertex
+		// 		vec3 CP = P - center;  // Vecteur du point par rapport au centre
+
+		// 		// Calcul de la distance du point à l'axe Y (distance dans le plan XY)
+		// 		float distanceToAxis = length(vec2(CP.x, CP.z));  // Distance en XZ
+		// 		float distanceNorm = distanceToAxis / size;
+		// 		float rotationAngle = intense * 3.1415926535897932384626433832795 * 0.5 * distanceNorm;  // Rotation en fonction de la distance
+
+		// 		// Calcul du signe de la rotation : positive ou négative (rotation horaire ou anti-horaire)
+		// 		float signRotation = -sign(CP.x);  // Rotation dans le sens positif ou négatif
+
+		// 		// Appliquer la rotation dans le plan XY
+		// 		float cosAngle = cos(rotationAngle * signRotation);
+		// 		float sinAngle = sin(rotationAngle * signRotation);
+
+		// 		// Nouvelle position après rotation autour de l'axe Y
+		// 		vec3 rotatedPosition;
+		// 		rotatedPosition.x = CP.x * cosAngle - CP.z * sinAngle;  // Rotation en X
+		// 		rotatedPosition.y = P.y;  // Pas de changement en Y
+		// 		rotatedPosition.z = CP.x * sinAngle + CP.z * cosAngle;  // Rotation en Z
+
+		// 		// Nouvelle position après avoir réajusté par rapport au centre
+		// 		vec3 newPosition = rotatedPosition + center;
+
+		// 		// Appliquer la projection
+		// 		vec4 p = vec4(newPosition, 1.);
+		// 		gl_Position = worldViewProjection * p;
+		// 	}
+		// `;
+
+		// Effect.ShadersStore['customFragmentShader'] = `
+		// 	precision highp float;
+
+		// 	void main() {
+		// 		vec3 color = vec3(1.0,0.0,0.0);
+		// 		gl_FragColor = vec4(color, 1.0);
+		// 	}
+		// `;
+
+		Effect.ShadersStore['customFragmentShader'] = `
+			precision highp float;
+
+			varying vec3 vNormal;
+
+			void main(void) {
+
+				vec3 normalColor = normalize(vNormal) * 0.5 + 0.5;
+				gl_FragColor = vec4( normalColor, 1. );
+			}
+		`;
+
+		Effect.ShadersStore['customVertexShader'] = `
+			precision highp float;
+
+			// Attributes
+			attribute vec3 position;
+			attribute vec3 normal;
+			attribute vec2 uv;
+
+			// Uniforms
+			uniform mat4 world;
+			uniform mat4 worldViewProjection;
+
+			// Varying
+			varying vec3 vNormal;
+
+			void main(void) {
+
+				vNormal = normalize(mat3(world) * normal);
+
+				float size = 0.825;
+				vec3 center = vec3(0, 0, 0);  // Centre du cylindre
+				vec3 axe = vec3(0, 1, 0);  // Axe de rotation (Y)
+				float intense = 0.01;  // Intensité de la rotation
+				vec3 P = position;  // Position du vertex
+				vec3 CP = P - center;  // Vecteur du point par rapport au centre
+
+				// Calcul de la distance du point à l'axe Y (distance dans le plan XY)
+				float distanceToAxis = length(vec2(CP.x, CP.z));  // Distance en XZ
+				float distanceNorm = distanceToAxis / size;
+				float rotationAngle = intense * 3.1415926535897932384626433832795 * 0.5 * distanceToAxis;  // Rotation en fonction de la distance
+
+				// Calcul du signe de la rotation : positive ou négative (rotation horaire ou anti-horaire)
+				float signRotation = -sign(P.x);  // Rotation dans le sens positif ou négatif
+
+				// Appliquer la rotation dans le plan XY
+				float cosAngle = cos(rotationAngle * signRotation);
+				float sinAngle = sin(rotationAngle * signRotation);
+
+				// Nouvelle position après rotation autour de l'axe Y
+				vec3 rotatedPosition;
+				rotatedPosition.x = CP.x * cosAngle - CP.z * sinAngle;  // Rotation en X
+				rotatedPosition.y = P.y;  // Pas de changement en Y
+				rotatedPosition.z = CP.x * sinAngle + CP.z * cosAngle;  // Rotation en Z
+
+				// Nouvelle position après avoir réajusté par rapport au centre
+				vec3 newPosition = rotatedPosition + center;
+
+				// Appliquer la projection
+				vec4 p = vec4(newPosition, 1.);
+				gl_Position = worldViewProjection * p;
+
+				// gl_Position = worldViewProjection * vec4(position, 1.0);
+
+			}
+		`;
+
+		shieldBaseMesh.convertToFlatShadedMesh();
+		shieldBaseMesh.computeWorldMatrix(true);
+		console.log(Effect.ShadersStore);
+		let shaderMaterial = new ShaderMaterial('custom', this.scene, 'custom', {
+			attributes: ['position', 'normal'],
+			uniforms: ['world', 'worldViewProjection']
 		});
-		shieldBaseMesh.material = paddleShader;
+		shieldBaseMesh.material = shaderMaterial;
 
 		const wallBaseMesh = MeshBuilder.CreateBox("wallBase", { width: 3, height: 1, depth: 1 }, this.scene);
 		wallBaseMesh.setEnabled(false);
@@ -54,10 +187,11 @@ class Game {
 		pillarBaseMesh.setEnabled(true);
 		pillarBaseMesh.setPivotPoint(Vector3.Zero());
 
-		const ballInstanceManager = new ThinInstanceManager(ballBaseMesh, 1000, 50, 100);
-		const paddleInstanceManager = new ThinInstanceManager(paddleBaseMesh, 100, 50, 100);
-		const wallInstanceManager = new ThinInstanceManager(wallBaseMesh, 100, 50, 100);
-		const pillarInstanceManager = new ThinInstanceManager(pillarBaseMesh, 100, 50, 100);
+		const ballInstanceManager = new ThinInstanceManager(ballBaseMesh, 1, 50, 100);
+		const paddleInstanceManager = new ThinInstanceManager(paddleBaseMesh, 1, 50, 100);
+		const shieldInstanceManager = new ThinInstanceManager(shieldBaseMesh, 1, 50, 100);
+		const wallInstanceManager = new ThinInstanceManager(wallBaseMesh, 1, 50, 100);
+		const pillarInstanceManager = new ThinInstanceManager(pillarBaseMesh, 1, 50, 100);
 
 		this.ecs = new ECSManager();
 		this.wsManager = new WebSocketManager("ws://localhost:8080");
@@ -69,15 +203,16 @@ class Game {
 		this.ecs.addSystem(new ThinInstanceSystem(
 			ballInstanceManager,
 			paddleInstanceManager,
+			shieldInstanceManager,
 			wallInstanceManager,
 			pillarInstanceManager,
 			this.camera
 		));
 
 		const config: GameTemplateConfig = {
-			numberOfPlayers: 100,
+			numberOfPlayers: 1,
 			numberOfBalls: 50,
-			arenaRadius: calculateArenaRadius(100),
+			arenaRadius: calculateArenaRadius(2),
 			numPillars: 100,
 			numWalls: 100
 		};
