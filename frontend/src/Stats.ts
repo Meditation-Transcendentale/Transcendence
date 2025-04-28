b7e1fd42a85d1081c6320e2861187c9008cd9a7b
Ilian Habiby <ihabiby@z2r11p4.42lyon.fr>
2025-04-28 17:33:11 +0200

    merge from main

--- frontend/src/Stats.ts
+++ frontend/src/Stats.ts
@@ -25,12 +25,12 @@ class Stats {
 		document.getElementById("stats-username").innerHTML = playerName;
 		document.getElementById("classic-menu")?.addEventListener("click", (e) => {
 			document.getElementById("stats-container").innerHTML = "";
-			// this.statsRequest(playerName, "classic")
-			// 	.then((response) => {
-			// 		this.parseResponse(response);
-			// 	})
-			// 	.catch((error) => { console.log(error) });
-			console.log("Stats Handler");
+			this.statsRequest(playerName, "classic")
+				.then((response) => {
+					this.parseResponse(response);
+				})
+				.catch((error) => { console.log(error) });
+			//console.log("Stats Handler");
 		});
 		document.getElementById("br-menu")?.addEventListener("click", (e) => {
 			document.getElementById("stats-container").innerHTML = "";
@@ -70,10 +70,11 @@ class Stats {
 	}
 
 	private async statsRequest(username: string, mode: string) {
-		const response = await fetch("https://localhost:3000/stats/player/" + mode + "/:" + username, {
+		const response = await fetch("https://localhost:3000/stats/player/" + username + "/" + mode, {
 			method: 'GET',
 			headers: {
 				'Accept': 'application/json',
+				'Content-Type': 'application/json'
 			},
 			credentials: 'include',
 		});
@@ -89,13 +90,15 @@ class Stats {
 	}
 
 	private parseResponse(response: any) {
-		const obj = JSON.parse(response.message.stats);
+		console.log(response);
+		const obj = JSON.parse(response);
+		console.log(obj);
 
-		let text = "<table id='stats-table'>"
-		for (let x in obj) {
-			text += "<tr><td>" + x.name + "</td><td>" + x.value + "</td></tr>";
-		}
-		document.getElementById("stats-container").innerHTML = text;
+		//let text = "<table id='stats-table'>"
+		//for (let x in obj) {
+		//	text += "<tr><td>" + x.name + "</td><td>" + x.value + "</td></tr>";
+		//}
+		//document.getElementById("stats-container").innerHTML = text;
 	}
 }
 
