8da6fbb2c2aaeae47ae4cb58ee3ea5b2bbed60f7
Camille Assie <cassie@z1r2p2.42lyon.fr>
2025-07-29 14:32:08 +0200

    fix

--- frontend/src/pongbr/systems/InputSystem.ts
+++ frontend/src/pongbr/systems/InputSystem.ts
@@ -51,7 +51,7 @@ export class InputSystem extends System {
 			if (leftPressed && !rightPressed) this.move = 1;
 			else if (rightPressed && !leftPressed) this.move = -1;
 
-			//offsetChange = this.move * 1.2 / paddle.speed * dt;
+			//let offsetChange = this.move * 10 / paddle.speed * dt;
 			//paddle.offset = Scalar.Clamp(paddle.offset + offsetChange, -paddle.maxoffset, paddle.maxoffset);
 			//transform.rotation.y = paddle.baseRotation + paddle.offset;
 			if (this.move != this.lastSentMove) {
