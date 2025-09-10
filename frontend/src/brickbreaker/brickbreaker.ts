a8f0ebb8859b5def9c0353e3f8f9d800c4009cba
Romain Jacq <rjacq@z3r3p4.42lyon.fr>
2025-09-10 14:51:31 +0200

    feat: add reset brick breaker game

--- frontend/src/brickbreaker/brickbreaker.ts
+++ frontend/src/brickbreaker/brickbreaker.ts
@@ -64,6 +64,18 @@ export class BrickBreaker {
 			return;
 		}
 
+		if (this.bricks.length > 0){
+			this.bricks.forEach(layer => {
+				layer.forEach(brick => {
+					brick.setEnabled(false);
+				});
+			});
+		}
+		this.layers = Math.ceil((Math.random() * 5) + 1);
+		this.cols = Math.ceil((Math.random() * 5) + 1);
+		this.ball.bricksLeft = this.layers * this.cols;
+		console.log("bricks :::: ", this.ball.bricksLeft);
+		this.bricks = this.generateBricks(10, this.layers, this.cols);
 
 		this.lastTime = performance.now();
 
@@ -82,6 +94,7 @@ export class BrickBreaker {
 			console.log("BrickBreaker removed from render loop");
 		}
 		this.player.disableInput();
+		this.reset();
 	}
 
 	private update(): void {
@@ -94,10 +107,12 @@ export class BrickBreaker {
 	}
 
 	public reset(): void {
-		this.ball.updatePosition(0, 1);
-		this.ball.setVelocity(new Vector3(0, 0, 0));
+		console.log("reset BrickBreaker");
+		// this.ball.updatePosition(0, 1);
+		// this.ball.setVelocity(new Vector3(0, 0, 0));
+		this.ball.reset();
+		this.player.reset();
 		this.lastTime = performance.now();
-
 	}
 
 	private setupCamera() {
