4e81f7634cf282ac769dc0092a1ff9a381045d64
Ilian Habiby <ihabiby@z1r2p4.42lyon.fr>
2025-10-06 11:04:05 +0200

    ouai before merge

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,71 +1,63 @@
+import { App3D } from "./3d/App";
+import { NotificationManager } from "./spa/NotificationManager";
+import { Popup } from "./spa/Popup";
+import Router from "./spa/Router";
+import { postRequest } from "./spa/requests";
 
+//async function init() {
+//	console.log("Page load with url: ", window.location.href.substring(window.location.origin.length));
+//
+//	Router.nav(window.location.href.substring(window.location.origin.length), false, false);
+//}
+//
 
-import { gameManager } from "./game/GameManager";
-import { routeManager } from "./route/RouteManager";
-import { sceneManager } from "./scene/SceneManager";
-import { streamManager } from "./stream/StreamManager";
-
-let loader: HTMLElement;
+let loader;
 
 async function init() {
 	console.log("Page load with url: ", window.location.href.substring(window.location.origin.length));
 
-	// createUser();
-	// createHtmlManager();
-	streamManager.builder.connect();
-	await sceneManager.loadMandatory();
-	routeManager.nav(window.location.href.substring(window.location.origin.length), false, true);
-	await gameManager.init();
+	const ws = new WebSocket(`wss://${window.location.hostname}:7000/ws`);
+	ws.onopen = () => console.log('Connected securely via WSS');
+	ws.onmessage = (event) => {
+		console.log(event.data);
+		if (event.data === 'reload') window.location.reload();
+	};
+
+	//Router.AUTHENTIFICATION = false;
+	// let App3D = await import("./3d/App.ts").default();
+	// let	NotificationManager = await import("./spa/NotificationManager").default();
+	// let Popup = await import("./spa/Popup.ts").default();
+	// let Router = await import("./spa/Router.ts").default();
+	// let postRequest = await import("./spa/requests.ts").postRequest;
 
 
-
-
-
-	// const ws = new WebSocket(`wss://${window.location.hostname}:7000/ws`);
-	// ws.onopen = () => console.log('Connected securely via WSS');
-	// ws.onmessage = (event) => {
-	// 	console.log(event.data);
-	// 	if (event.data === 'reload') window.location.reload();
-	// };
-
-
-
-	// await App3D.init()
-	// // .then(() => {
-	// Router.nav(window.location.href.substring(window.location.origin.length), false, false);
-	// App3D.run()
-	// // })
-	//
-	// window.onbeforeunload = () => {
-	// 	App3D.dispose();
-	// }
-	//
-	// NotificationManager.setEnable(true);
-
-	// window.addEventListener('keydown', (e) => {
-	// 	if (e.key == 'Escape') {
-	// 		Router.nav('/home', false, true)
-	// 		Popup.removePopup();
-	// 	}
-	// 	if (e.key == "p") {
-	// 		NotificationManager.addText(`${performance.now()}`)
-	// 	}
-	// 	//if (e.key == "") {
-	// 	//	postRequest("friends/add", { inputUsername: "Erwan"});
-	// 	//}
+	await App3D.init()
+	// .then(() => {
+	Router.nav(window.location.href.substring(window.location.origin.length), false, false);
+	App3D.run()
 	// })
-	//
+
+	window.onbeforeunload = () => {
+		App3D.dispose();
+	}
+
+	NotificationManager.setEnable(true);
 
 	window.addEventListener('keydown', (e) => {
 		if (e.key == 'Escape') {
-			routeManager.nav('/home', false, true)
+			Router.nav('/home', false, true)
+			Popup.removePopup();
 		}
+		if (e.key == "p") {
+			NotificationManager.addText(`${performance.now()}`)
+		}
+		//if (e.key == "") {
+		//	postRequest("friends/add", { inputUsername: "Erwan"});
+		//}
 	})
 
 
-
 	loader!.remove();
-	sceneManager.run();
 }
 
 
@@ -113,4 +105,5 @@ window.addEventListener("DOMContentLoaded", () => {
 	addLoader();
 	checkOnlyOneTab();
 })
+
 
