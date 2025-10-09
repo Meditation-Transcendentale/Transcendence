78030a9af23bd11fefec652840f66dc38f0a6986
Romain Jacq <rjacq@z1r2p4.42lyon.fr>
2025-10-09 11:18:31 +0200

    fix: leaderboard and pong start handling

--- frontend/src/game/GameUI.ts
+++ frontend/src/game/GameUI.ts
@@ -668,6 +668,7 @@ interface EndingHtmlReference {
 	soloModeContainer: HTMLDivElement;
 	result: HTMLSpanElement;
 	leaderboard: HTMLDivElement;
+	players: HTMLDivElement;
 }
 
 type Player = {
@@ -689,7 +690,8 @@ class EndingModule implements GameUIModule {
 			dualModeContainer: div.querySelector('.dual-mode') as HTMLDivElement,
 			soloModeContainer: div.querySelector('.solo-mode') as HTMLDivElement,
 			result: div.querySelector("#result-label") as HTMLSpanElement,
-			leaderboard: div.querySelector("#players-list") as HTMLDivElement
+			leaderboard: div.querySelector("#leaderboard") as HTMLDivElement,
+			players: div.querySelector("#players-list") as HTMLDivElement
 		};
 	}
 
@@ -715,7 +717,8 @@ class EndingModule implements GameUIModule {
 			else
 				this.ref.result.innerHTML = '';
 
-			this.ref.leaderboard.innerHTML = '';
+			// this.ref.leaderboard.style.display = 'flex';
+			this.ref.players.innerHTML = '';
 			
 			for (let i = 0; i < this.leaderboard.length; i++) {
 				const row = document.createElement('tr');
@@ -732,9 +735,10 @@ class EndingModule implements GameUIModule {
 				scoreCell.textContent = this.leaderboard[i].highscore.toString();
 				row.appendChild(scoreCell);
 				
-				this.ref.leaderboard.appendChild(row);
+				this.ref.players.appendChild(row);
 			}
 		} else {
+			this.ref.leaderboard.style.display = 'none';
 			this.ref.dualModeContainer.style.display = 'flex';
 			this.ref.soloModeContainer.style.display = 'none';
 
